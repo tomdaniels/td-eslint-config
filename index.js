4c8a08efd6ba3dc4f8e6a29c9fef6f6abe2463a9
tomdaniels <thomsondaniels@hotmail.com>
2018-08-25 17:20:58 +1000

    fix: no unused variable

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+      },
+    ],
+    'no-negated-condition': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react/no-direct-mutation-state': 'error',
+    'id-length': [
+      'error',
+      {
+        min: 3,
+        properties: 'never',
+        exceptions: ['_'],
+      },
+    ],
+    'padded-blocks': 'off',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true,
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    curly: ['error', 'all'],
+  },
+};
