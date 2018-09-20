e0f6e7938d7235ff7b86045c10f468d1563590bb
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 17:35:07 +1000

    feat: arrowParens as needed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
       {
         singleQuote: true,
         trailingComma: 'all',
-        arrowParens: 'always',
+        arrowParens: 'as-needed',
       },
     ],
     'no-negated-condition': 'error',
