3fab0099b04942c105395039c3e99c83bbea6d20
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 19:24:47 +1000

    fix: add global variables

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  env: {
+    'browser': true,
+    'node': true,
+  },
   rules: {
-      semi: [2, "always"]
-  }
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    'no-negated-condition': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react/no-direct-mutation-state': 'error',
+    'id-length': [
+      'error',
+      {
+        min: 3,
+        properties: 'never',
+        exceptions: ['_'],
+      },
+    ],
+    'padded-blocks': 'off',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allowAfterThis: true,
+      },
+    ],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'arrow-parens': [
+      'error',
+      'as-needed',
+    ],
+    curly: ['error', 'all'],
+  },
 };
