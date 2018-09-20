a4c16dde75017d8dcba7a39bf36351c5e4fcd453
tomdaniels <thomsondaniels@hotmail.com>
2018-09-20 18:20:35 +1000

    test

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
-  parser: 'babel-eslint',
-  plugins: ['prettier'],
-  extends: ['airbnb', 'prettier', 'prettier/react'],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
-      },
-    ],
-    'no-negated-condition': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.js', '.jsx'],
-      },
-    ],
-    'react/no-direct-mutation-state': 'error',
-    'id-length': [
-      'error',
-      {
-        min: 3,
-        properties: 'never',
-        exceptions: ['_'],
-      },
-    ],
-    'padded-blocks': 'off',
-    'no-underscore-dangle': [
-      'error',
-      {
-        allowAfterThis: true,
-      },
-    ],
-    'no-unused-vars': [
-      'error',
-      {
-        vars: 'all',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-      },
-    ],
-    curly: ['error', 'all'],
-  },
+      semi: [2, "always"]
+  }
 };
